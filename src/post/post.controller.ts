be12134a568be7abe8edc6b68ae14cd8e20fdc9e
KD63799 <aninach.beg@gmail.com>
2025-04-22 11:24:04 +0200

    crud post done

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,13 +1,18 @@
-import {Body, Controller, Post, Req, UseGuards} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, Req, UseGuards} from '@nestjs/common';
 import {PostService} from "./post.service";
 import {AuthGuard} from "@nestjs/passport";
 import {CreatePostDto} from "./dto/createPostDto";
 import {Request} from "express";
+import {UpdatePostDto} from "./dto/updatePostDto";
 
 @Controller('post')
 export class PostController {
     constructor(private readonly postService: PostService) {}
 
+    @Get()
+    getAll() {
+        return this.postService.getAll()
+    }
 
     @UseGuards(AuthGuard("jwt"))
     @Post("create")
@@ -16,4 +21,23 @@ export class PostController {
         const userId = request.user["userId"]
         return this.postService.create(createPostDto, userId)
     }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Delete("delete/:id")
+    delete(@Param("id", ParseIntPipe) postId: number, @Req() request: Request) {
+        // @ts-ignore
+        const userId = request.user["userId"];
+        return this.postService.delete(postId, userId)
+    }
+
+
+    @UseGuards(AuthGuard("jwt"))
+    @Put("update/:id")
+    update(@Param("id", ParseIntPipe) postId: number, @Body() updatePostDto: UpdatePostDto,
+           @Req() request: Request) {
+        // @ts-ignore
+        const userId = request.user["userId"];
+        return this.postService.update(postId, userId, updatePostDto)
+    }
+
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,13 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import {ForbiddenException, Get, Injectable, NotFoundException} from '@nestjs/common';
 import {PrismaService} from "../prisma/prisma.service";
 import {CreatePostDto} from "./dto/createPostDto";
+import {UpdatePostDto} from "./dto/updatePostDto";
 
 @Injectable()
 export class PostService {
-    constructor( private readonly prismaService: PrismaService ) {}
-    async create(createPostDto: CreatePostDto, userId: any)   {
+    constructor(private readonly prismaService: PrismaService) {
+    }
+
+
+    @Get()
+    async getAll() {
+        return this.prismaService.post.findMany(
+            {
+                include: {
+                    user: {
+                        select: {
+                            username: true,
+                            email: true,
+                            password: false
+                        }
+                    },
+                    comments: {
+                        include: {
+                            user: {
+                                select: {
+                                    username: true,
+                                    email: true,
+                                    password: false
+                                }
+                            }
+                        }
+                    },
+                }
+            }
+        );
+    }
+
+    async create(createPostDto: CreatePostDto, userId: any) {
         const {body, title} = createPostDto;
         await this.prismaService.post.create({data: {body, title, userId}})
         return {data: "Post successfully created"}
+    }
+
+    async delete(postId: number, userId: any) {
+        const post = await this.prismaService.post.findUnique({where: {postId}});
+        if (!post) throw new NotFoundException("Post not found")
+        if (post.userId != userId) throw new ForbiddenException("Forbidden Action")
+        await this.prismaService.post.delete({where: {postId}});
+        return {data: "Post deleted"}
+    }
+
+
+    async update(postId: number, userId: any, updatePostDto: UpdatePostDto) {
+        const post = await this.prismaService.post.findUnique({where: {postId}});
+        if (!post) throw new NotFoundException("Post not found")
+        if (post.userId != userId) throw new ForbiddenException("Forbidden Action")
+        await this.prismaService.post.update({where: {postId}, data: {...updatePostDto}})
+        return {data: "Post updated"}
     }
 }
